cbf60dc6141158930880cc4147d982fbd8ad028d
Stefan Karlsson <phtefan.karlsson@gmail.com>
2024-02-19 11:51:05 +0100

    Tutorial part two and three, Add border and set game boundaries.

--- main.py
+++ main.py
@@ -18,9 +18,82 @@ class Sprite(turtle.Turtle):
         self.goto(startx, starty)
         self.speed = 1
 
+    def move(self):
+        self.fd(self.speed)
 
-player = Sprite("triangle", "white", 0, 0)
+        if self.xcor() > 340:
+            self.setx(340)
+            self.rt(60)
+
+        if self.xcor() < -340:
+            self.setx(-340)
+            self.rt(60)
+
+        if self.ycor() > 340:
+            self.sety(340)
+            self.rt(60)
+
+        if self.ycor() < -340:
+            self.sety(-340)
+            self.rt(60)
+
+class Player(Sprite):
+    def __init__(self,spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 4
+        self.lives = 3
+
+    def turn_left(self):
+        self.lt(45)
+
+    def turn_right(self):
+        self.rt(45)
+
+    def accelerate(self):
+        self.speed += 1
+
+    def decelerate(self):
+        self.speed -= 1
+
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+
+    def draw_border(self):
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-350, 350)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.forward(700)
+            self.pen.right(90)
+        self.pen.penup()
+        self.pen.ht()
 
 
-delay = ("Press enter to finish. > ")
+
+game = Game()
+
+game.draw_border()
+
+player = Player("triangle", "white", 0, 0)
+
+turtle.onkey(player.turn_left, "a")
+turtle.onkey(player.turn_right, "d")
+turtle.onkey(player.accelerate, "w")
+turtle.onkey(player.decelerate, "s")
+turtle.listen()
+
+
+while True:
+    player.move()
+
+
+
 
